f972346f5f434b8736ba6cf51060d9c313005883
marco-beraldo <marco.beraldo@gmail.com>
2025-03-22 19:08:59 +0100

    added iubenda

--- src/wp-theme/footer.php
+++ src/wp-theme/footer.php
@@ -11,7 +11,7 @@
    </div>
 
     <!-- IUBENDA -->
-      <script type="text/javascript" src="//embeds.iubenda.com/widgets/ad10d836-343c-4fc1-933e-5b1048ccce4a.js"></script>
+    <script type="text/javascript" src="//embeds.iubenda.com/widgets/ad10d836-343c-4fc1-933e-5b1048ccce4a.js"></script>
    <?php wp_footer(); ?>
 
    </body>
